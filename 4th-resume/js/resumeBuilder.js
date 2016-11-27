50c1a7d3d7555efbd7737e036474dba2c93e01b9
Edgar Quintero <edarioq@gmail.com>
2016-11-26 22:29:25 -0500

    Started new lesson

--- 4th-resume/js/resumeBuilder.js
+++ 4th-resume/js/resumeBuilder.js
@@ -1,3 +1,93 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+//  Employer, title, location, dates worked and description
+var work = {
+  jobs: [{
+    "employer": "Webprodo",
+    "title": "Full Stack Developer",
+    "location": "Remote",
+    "dates": "2015 - Present",
+    "description": "Small company, design, tecnology, marketing, and data."
+  }, {
+    "employer": "BlackSip",
+    "title": "SEO Specialist",
+    "location": "Bogotá",
+    "dates": "2015",
+    "description": "Ecommerce startup."
+  }, {
+    "employer": "GroupM",
+    "title": "SEO Executive",
+    "location": "Bogotá",
+    "dates": "2013-2014",
+    "description": "Multinational digital marketing holding."
+  }],
+};
+
+// Title, dates and description strings, and an images array with URL strings for project images.
+var projects = {
+  projects: [{
+    "title": "Prohumanos Consultores LTDA",
+    "dates": "2016",
+    "description": "Custom website development with HTML, CSS, JS, PHP, SASS, Gulp, Git, and WordPress",
+    "images": ["", ""],
+  }, {
+    "title": "Life 72",
+    "dates": "2016",
+    "description": "Custom website development with HTML, CSS, JS, PHP, SASS, Gulp, Git, and WordPress",
+    "images": ["", ""],
+  }, {
+    "title": "Ruber Almanza",
+    "dates": "2016",
+    "description": "Custom website development with HTML, CSS, JS, PHP, SASS, Gulp, Git, and WordPress",
+    "images": ["", ""],
+  }, {
+    "title": "Alliance BJJ",
+    "dates": "2016",
+    "description": "Custom website development with HTML, CSS, JS, PHP, SASS, Gulp",
+    "images": ["", ""],
+  }]
+};
+
+// Name, role, welcomeMessage, and biopic strings, contacts object and skills array of skill strings.
+// The contacts object should contain a mobile number, email address, github username, twitter handle and location.
+var bio = {
+  "name": "Edgar Quintero",
+  "role": "Full Stack Developer",
+  "welcomeMessage": "Greetings!",
+  "biopci": "images/me.jpg",
+  "contacts": {
+    "mobile": "316.520.8890",
+    "email": "edarioq@gmail.com",
+    "github": "edarioq",
+    "twitter": "edarioq",
+    "location": "Bogotá",
+  },
+  "skills": ["HTML", "CSS", "JavaScript", "SASS", "Gulp", "Git", "WordPress", "SEO", "Google Analytics"]
+};
+
+// Also contains an online Courses array.
+// Each object in the onlineCourses array should contain a title, school, dates and url strings
+var education = {
+  "schoolName": "Politecnico Gran Colombiano",
+  "title": "Systems Engineer",
+  "year": "2010-2012",
+  "onlineCourses": [{
+    "title": "Front End Developer - In progress",
+    "school": "Udacity",
+    "dates": "2016",
+    "url": "//www.udacity.com",
+  }, {
+    "title": "Learning how to Learn",
+    "school": "Coursera",
+    "dates": "2016",
+    "url": "//www.coursera.org",
+  }, {
+    "title": "JavaScript",
+    "school": "Code Academy",
+    "dates": "2016",
+    "url": "//www.codecademy.com",
+  }, {
+    "title": "Shaping up with Angular",
+    "school": "Code School",
+    "dates": "2015",
+    "url": "//www.codeschool.com/courses/shaping-up-with-angular-js",
+  }],
+};
