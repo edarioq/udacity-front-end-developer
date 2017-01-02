7fb12bf1f90d7ef516b877294035287ff366a786
Edgar Quintero <edarioq@gmail.com>
2017-01-01 22:19:54 -0500

    Implement reviewer feedback

--- 4th-resume/js/resumeBuilder.js
+++ 4th-resume/js/resumeBuilder.js
@@ -14,7 +14,26 @@ var bio = {
     },
     "welcomeMessage": "Writing beautiful code is as much a science as it is an art form.",
     "skills": ["HTML", "CSS", "JavaScript", "jQuery", "SASS", "Gulp", "Git", "WordPress", "SEO", "Google Analytics"],
-    "biopic": "images/me.jpg"
+    "biopic": "images/me.jpg",
+    "display": function() {
+
+        $('#header').prepend(HTMLheaderRole.replace('%data%', bio.role));
+        $('#header').prepend(HTMLheaderName.replace('%data%', bio.name));
+
+        $.each(bio.contacts, function(key, value) {
+            $('#topContacts').append(HTMLcontactGeneric.replace('%contact%', key).replace('%data%', value));
+            $('#footerContacts').append(HTMLcontactGeneric.replace('%contact%', key).replace('%data%', value));
+        });
+
+        $('#header').append(HTMLbioPic.replace('%data%', bio.biopic));
+        $('#header').append(HTMLwelcomeMsg.replace('%data%', bio.welcomeMessage));
+
+        $('#header').append(HTMLskillsStart);
+        $.each(bio.skills, function(value) {
+            $('#skills').append(HTMLskills.replace('%data%', bio.skills[value]));
+        });
+
+    }
 };
 
 var education = {
@@ -22,14 +41,14 @@ var education = {
         "name": "Politécnico Grancolombiano",
         "location": "Bogotá",
         "degree": "Systems Engineer",
-        "majors": "N/A",
+        "majors": ["N/A"],
         "dates": "2012-2014",
         "url": "https://www.poli.edu.co",
     }, {
         "name": "Norcross High School",
         "location": "Atlanta, GA",
         "degree": "N/A",
-        "majors": "N/A",
+        "majors": ["N/A"],
         "dates": "2000-2004",
         "url": "https://www.google.com",
     }],
@@ -43,7 +62,36 @@ var education = {
         "school": "CodeAcademy",
         "dates": "June 2015",
         "url": "https://www.codecademy.com/learn/javascript",
-    }]
+    }],
+    "display": function() {
+        // Education
+        $.each(education.schools, function(i) {
+            $('#education').append(HTMLschoolStart);
+
+            var sName = HTMLschoolName.replace('%data%', education.schools[i].name);
+            var sDegree = HTMLschoolDegree.replace('%data%', education.schools[i].degree);
+            var sDates = HTMLschoolDates.replace('%data%', education.schools[i].dates);
+            var sLocation = HTMLschoolLocation.replace('%data%', education.schools[i].location);
+            var sMajors = HTMLschoolMajor.replace('%data%', education.schools[i].majors);
+
+            $('.education-entry:last').append(sName + sDegree + sDates + sLocation + sMajors);
+
+        });
+
+        // Online courses
+        $('#education').append(HTMLonlineClasses); //Header
+
+        $.each(education.onlineCourses, function(i) {
+            $('#education').append(HTMLschoolStart);
+
+            var courseTitle = HTMLonlineTitle.replace('%data%', education.onlineCourses[i].title);
+            var courseSchool = HTMLonlineSchool.replace('%data%', education.onlineCourses[i].school);
+            var courseDates = HTMLonlineDates.replace('%data%', education.onlineCourses[i].dates);
+            var courseUrl = HTMLonlineURL.replace('#', education.onlineCourses[i].url).replace('%data%', education.onlineCourses[i].url);
+
+            $('.education-entry:last').append(courseTitle + courseSchool + courseDates + courseUrl);
+        });
+    }
 };
 
 var work = {
@@ -52,13 +100,16 @@ var work = {
         "title": "Web Developer",
         "location": "Remote",
         "dates": "December 2015 - Present",
-        "description": "Design, tecnology, digital strategy, and data services to both small local businesses and various startups around the world."
+        "description": "Design, tecnology, digital strategy, and data " +
+        "services to both small local businesses and various startups " +
+        "around the world."
     }, {
         "employer": "BlackSip",
         "title": "SEO Strategist",
         "location": "Bogotá",
         "dates": "June 2015 - November 2015",
-        "description": "Ecommerce digital marketing agency and technology focused startup."
+        "description": "Ecommerce digital marketing agency and technology " +
+        "focused startup."
     }, {
         "employer": "GroupM",
         "title": "SEO Executive",
@@ -70,12 +121,31 @@ var work = {
         "title": "SEO Specialist",
         "location": "Bogotá",
         "dates": "June 2010 - July 2013",
-        "description": "Colombian Multinational Company specializing in Software, Infrastructure, Cloud Computing, Business Intelligence, and of couse Digital."
-    }]
+        "description": "Colombian Multinational Company specializing in " +
+        "Software, Infrastructure, Cloud Computing, Business Intelligence " +
+        "and of couse Digital."
+    }],
+    "display": function() {
+
+        $.each(work.jobs, function(job) {
+            $('#workExperience').append(HTMLworkStart);
+
+            var myJobs = work.jobs[job];
+
+            var myEmployer = HTMLworkEmployer.replace('%data%', myJobs.employer);
+            var myTitle = HTMLworkTitle.replace('%data%', myJobs.title);
+            var myDates = HTMLworkDates.replace('%data%', myJobs.dates);
+            var myLocation = HTMLworkLocation.replace('%data%', myJobs.location);
+            var myDesc = HTMLworkDescription.replace('%data%', myJobs.description);
+
+            $('.work-entry:last').append(myEmployer + ' ' + myTitle + myDates + myLocation + myDesc);
+        });
+
+    }
 };
 
 var projects = {
-    myProject: [{
+    projects: [{
         "title": "Prohumanos Consultores LTDA",
         "dates": "December 2016",
         "description": "Custom website coded in HTML, CSS (SASS), JS (jQuery), PHP, Gulp, Git, and WordPress.",
@@ -95,115 +165,34 @@ var projects = {
         "dates": "September 2016",
         "description": "Custom website coded in HTML, CSS (SASS), JS (jQuery), PHP, Gulp, Git, and WordPress",
         "images": ["images/project-ruber-1.png", "images/project-ruber-2.png", "images/project-ruber-3.png"],
-    }]
-};
+    }],
+    "display": function() {
 
-/*
- * Build and show bio
- */
+        $.each(projects.projects, function(project) {
+            $('#projects').append(HTMLprojectStart);
 
-function displayBio() {
+            var projectss = projects.projects[project];
 
-    $('#header').prepend(HTMLheaderRole.replace('%data%', bio.role));
-    $('#header').prepend(HTMLheaderName.replace('%data%', bio.name));
+            var projectTitle = HTMLprojectTitle.replace('%data%', projectss.title);
+            var projectDates = HTMLprojectDates.replace('%data%', projectss.dates);
+            var projectDescription = HTMLprojectDescription.replace('%data%', projectss.description);
 
-    $.each(bio.contacts, function(key, value) {
-        $('#topContacts').append(HTMLcontactGeneric.replace('%contact%', key).replace('%data%', value));
-        $('#footerContacts').append(HTMLcontactGeneric.replace('%contact%', key).replace('%data%', value));
-    });
+            $('.project-entry:last').append(projectTitle + projectDates + projectDescription);
 
-    $('#header').append(HTMLbioPic.replace('%data%', bio.biopic));
-    $('#header').append(HTMLwelcomeMsg.replace('%data%', bio.welcomeMessage));
+            $.each(projectss.images, function(i) {
+                var theImage = HTMLprojectImage.replace('%data%', projectss.images[i]);
+                $('.project-entry:last').append(theImage);
+            });
 
-    $('#header').append(HTMLskillsStart);
-    $.each(bio.skills, function(value) {
-        $('#skills').append(HTMLskills.replace('%data%', bio.skills[value]));
-    });
-
-}
-displayBio();
-
-/*
- * Build and show work
- */
-function displayWork() {
-    $('#workExperience').append(HTMLworkStart);
-
-    $.each(work.jobs, function(job) {
-        var myJobs = work.jobs[job];
-
-        var myEmployer = HTMLworkEmployer.replace('%data%', myJobs.employer);
-        var myTitle = HTMLworkTitle.replace('%data%', myJobs.title);
-        var myDates = HTMLworkDates.replace('%data%', myJobs.dates);
-        var myLocation = HTMLworkLocation.replace('%data%', myJobs.location);
-        var myDesc = HTMLworkDescription.replace('%data%', myJobs.description);
-
-        $('.work-entry').append(myEmployer + ' ' + myTitle + myDates + myLocation + myDesc);
-    });
-
-}
-displayWork();
-
-/*
- * Build and show projects
- */
-function displayProjects() {
-
-    $('#projects').append(HTMLprojectStart);
-
-    $.each(projects.myProject, function(project) {
-
-        var myProjects = projects.myProject[project];
-
-        var projectTitle = HTMLprojectTitle.replace('%data%', myProjects.title);
-        var projectDates = HTMLprojectDates.replace('%data%', myProjects.dates);
-        var projectDescription = HTMLprojectDescription.replace('%data%', myProjects.description);
-
-        $('.project-entry').append(projectTitle + projectDates + projectDescription);
-
-        $.each(myProjects.images, function(i) {
-            var theImage = HTMLprojectImage.replace('%data%', myProjects.images[i]);
-            $('.project-entry:last').append(theImage);
         });
 
-    });
+    }
+};
 
-}
-displayProjects();
-
-/*
- * Build and show education and online courses
- */
-function displayEducation() {
-    // Education
-    $('#education').append(HTMLschoolStart);
-
-    $.each(education.schools, function(i) {
-
-        var sName = HTMLschoolName.replace('%data%', education.schools[i].name);
-        var sDegree = HTMLschoolDegree.replace('%data%', education.schools[i].degree);
-        var sDates = HTMLschoolDates.replace('%data%', education.schools[i].dates);
-        var sLocation = HTMLschoolLocation.replace('%data%', education.schools[i].location);
-        var sMajors = HTMLschoolMajor.replace('%data%', education.schools[i].majors);
-
-        $('.education-entry').append(sName + sDegree + sDates + sLocation + sMajors);
-
-    });
-
-    $('.education-entry').append(HTMLonlineClasses);
-
-    // Online courses
-    $.each(education.onlineCourses, function(i) {
-
-        var courseTitle = HTMLonlineTitle.replace('%data%', education.onlineCourses[i].title);
-        var courseSchool = HTMLonlineSchool.replace('%data%', education.onlineCourses[i].school);
-        var courseDates = HTMLonlineDates.replace('%data%', education.onlineCourses[i].dates);
-        var courseUrl = HTMLonlineURL.replace('#', education.onlineCourses[i].url).replace('%data%', education.onlineCourses[i].url);
-
-        $('.education-entry').append(courseTitle + courseSchool + courseDates + courseUrl);
-    });
-}
-displayEducation();
+bio.display();
+work.display();
+projects.display();
+education.display();
 
 /*
  * Build and show the map
