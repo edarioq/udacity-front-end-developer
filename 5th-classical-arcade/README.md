7214ff0c9df7f6b93c2f3646173ab49a1895fb48
Edgar Quintero <edarioq@gmail.com>
2017-01-23 20:42:14 -0500

    Improved this keyword use

--- 5th-classical-arcade/README.md
+++ 5th-classical-arcade/README.md
@@ -1,35 +1,11 @@
 # Udacity FEND Arcade Game
 
-Functions written are **object-oriented** - either class functions (like Player and Enemy) or class prototype functions such as Enemy.prototype.checkCollisions, and appropriate use of the keyword 'this' in classes and class prototype functions that refer to the object the function is called upon.
+## How to run the app
+1. Download or clone the file to your desktop
+2. Double click `index.html` or drag it to your browser
+3. Enjoy!
 
-For detailed instructions, check out [guide](https://docs.google.com/document/d/1v01aScPjSWCCWQLIpFqvg3-vXLH2e8_SZQKC8jNO0Dc/pub?embedded=true).
+## How to play
+The objective is to move the player and reach the water without hitting any of the bugs.
 
-----------------------------
-## [Project Rubric](https://review.udacity.com/#!/projects/2696458597/rubric)
-
-### Error Free
-
-The game functions correctly and runs error free
-
-* Player can not move off screen
-* Vehicles cross the screen
-* Vehicle-player collisions happen logically (not too early or too late)
-* Vehicle-player collision resets the game
-* Something happens when player wins
-
-### Object Oriented code
-
-Game objects (player and vehicles) are implemented using JavaScript object-oriented programming features.
-
-### Documentation
-
-* A `README` file is included detailing all steps required to successfully run the application.
-* Comments are present and effectively explain longer code procedures. As a rule of thumb: describe what all custom functions and object methods do.
-* Code is formatted with consistent, logical, and easy-to-read formatting as described in the Udacity JavaScript Style Guide.
-
-### Additional functionality to the game beyond minimum requirements, for example:
-
-* Add collectible items on screen
-* Multiple vehicle types
-* Timed games
-* Be creative!
+Use the arrow keys `up`, `down`, `left`, or `right` to move the player.
--- 5th-classical-arcade/js/app.js
+++ 5th-classical-arcade/js/app.js
@@ -14,58 +14,56 @@ var Enemy = function(x, y, speed) {
 Enemy.prototype.update = function(dt) {
     // Movement is multiplied by the dt parameter so that
     // the game runs at the same speed for all computers.
-    dt = 1;
+    dt = 3;
 
-    bugOne.x = bugOne.x + (dt * bugOne.speed);
-    bugTwo.x = bugTwo.x + (dt * bugTwo.speed);
-    bugThree.x = bugThree.x + (dt * bugThree.speed);
+    this.x += (dt * this.speed);
 
+};
 
+Enemy.prototype.checkCollisions = function() {
     // Collision detection algorithm
     // https://developer.mozilla.org/en-US/docs/Games/Techniques/2D_collision_detection
-    for (i = 0; i < allEnemies.length; i++) {
-        var bugCollide = {
-            x: allEnemies[i].x,
-            y: allEnemies[i].y,
-            width: 50,
-            height: 50,
-        };
+    var bugCollide = {
+        x: this.x,
+        y: this.y,
+        width: 50,
+        height: 50,
+    };
 
-        var playerCollide = {
-            x: player.x,
-            y: player.y,
-            width: 50,
-            height: 50,
-        };
+    var playerCollide = {
+        x: player.x,
+        y: player.y,
+        width: 50,
+        height: 50,
+    };
 
-        if (bugCollide.x < playerCollide.x + playerCollide.width &&
-            bugCollide.x + bugCollide.width > playerCollide.x &&
-            bugCollide.y < playerCollide.y + playerCollide.height &&
-            bugCollide.height + bugCollide.y > playerCollide.y) {
+    if (bugCollide.x < playerCollide.x + playerCollide.width &&
+        bugCollide.x + bugCollide.width > playerCollide.x &&
+        bugCollide.y < playerCollide.y + playerCollide.height &&
+        bugCollide.height + bugCollide.y > playerCollide.y) {
 
-            player.x = 200;
-            player.y = 400;
-        }
+        player.x = 200;
+        player.y = 400;
     }
-
-    resetBug();
 };
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    this.resetBug();
+    this.checkCollisions();
 };
 
 // Reset bug location after it goes off screen
 // and give it a different speed
-function resetBug() {
-    for (i = 0; i < allEnemies.length; i++) {
-        if (allEnemies[i].x > 500) {
-            allEnemies[i].x = 0;
-            allEnemies[i].speed = Math.random() + 1.2;
-        }
+Enemy.prototype.resetBug = function resetBug() {
+
+    if (this.x > 500) {
+        this.x = -100;
+        this.speed = Math.random() + 1.2;
     }
-}
+
+};
 
 // Now write your own player class
 // This class requires an update(), render() and
@@ -80,49 +78,51 @@ Player.prototype.update = function(dt) {
     // Might not need this dt variable on player?
     // since it's not being used
     dt = 1;
-    player.win();
 };
 
 Player.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    this.win();
 };
 
 Player.prototype.handleInput = function(allowedKeys) {
 
     if (allowedKeys == 'up') {
-        player.y = player.y - 83;
+        this.y = this.y - 83;
     } else if (allowedKeys == 'down') {
-        player.y = player.y + 83;
+        this.y = this.y + 83;
     } else if (allowedKeys == 'left') {
-        player.x = player.x - 101;
+        this.x = this.x - 101;
     } else {
-        player.x = player.x + 101;
+        this.x = this.x + 101;
     }
 
     // Make sure player doesn't go out of bounds
-    if (player.x < 0) {
-        player.x = 0;
-    } else if (player.x > 400) {
-        player.x = 400;
-    } else if (player.y > 400) {
-        player.y = 400;
+    if (this.x < 0) {
+        this.x = 0;
+    } else if (this.x > 400) {
+        this.x = 400;
+    } else if (this.y > 400) {
+        this.y = 400;
     }
-
 };
 
 Player.prototype.win = function() {
-    if (player.y < 0) {
-        alert('NICE - You won!');
-        player.x = 200;
-        player.y = 400;
+    // Check if the player is on the top row
+    if (this.y <= -15) {
+        this.x = 200;
+        this.y = 400;
+        setTimeout(function() {
+            alert('NICE - You won!');
+        }, 200);
 
     }
 };
 
 // Now instantiate your objects.
 var bugOne = new Enemy(0, 60, Math.random() + 2);
-var bugTwo = new Enemy(0, 143, Math.random() + 3);
-var bugThree = new Enemy(0, 228, Math.random() + 1);
+var bugTwo = new Enemy(0, 143, Math.random() + 1);
+var bugThree = new Enemy(0, 228, Math.random() + 3);
 
 // Place all enemy objects in an array called allEnemies
 var allEnemies = [bugOne, bugTwo, bugThree];
