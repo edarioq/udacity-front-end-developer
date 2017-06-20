131e18de1d08cd4f3aa0cc8984a0f8e79c18a8c3
edarioq <edarioq@gmail.com>
2017-06-19 19:59:52 -0500

    Several fixes

--- cats-app/app.js
+++ cats-app/app.js
@@ -3,33 +3,37 @@
 /*
  * Model
  */
-var catData = [
-    {
-        name: 'Cat 1',
-        imgURL: 'imgs/cats-1.jpeg',
-        clicks: 0,
-    }, 
-    {
-        name: 'Cat 2',
-        imgURL: 'imgs/cats-2.jpeg',
-        clicks: 0,
-    },
-    {
-        name: 'Cat 3',
-        imgURL: 'imgs/cats-3.jpeg',
-        clicks: 0,
-    },
-    {
-        name: 'Cat 4',
-        imgURL: 'imgs/cats-4.jpeg',
-        clicks: 0,
-    },
-    {
-        name: 'Cat 5',
-        imgURL: 'imgs/cats-5.jpeg',
-        clicks: 0,
-    }
-];
+
+var catData = {
+    currentCat: null,
+    cats: [
+        {
+            name: 'Cat 1',
+            imgURL: 'imgs/cats-1.jpeg',
+            clicks: 0,
+        },
+        {
+            name: 'Cat 2',
+            imgURL: 'imgs/cats-2.jpeg',
+            clicks: 0,
+        },
+        {
+            name: 'Cat 3',
+            imgURL: 'imgs/cats-3.jpeg',
+            clicks: 0,
+        },
+        {
+            name: 'Cat 4',
+            imgURL: 'imgs/cats-4.jpeg',
+            clicks: 0,
+        },
+        {
+            name: 'Cat 5',
+            imgURL: 'imgs/cats-5.jpeg',
+            clicks: 0,
+        },
+    ]
+}
 
 
 /*
@@ -38,15 +42,31 @@ var catData = [
 
 var view = {
 
-    setCurrentCat: function(cat) {
-        var currentCat = cat;
-        return cat;
+    loadCats: function () {
+
+        var catForm = document.getElementById('cats-form');
+        var catImage = document.getElementById('cat-pic');
+
+        for (i = 0; i < catData.cats.length; i++) {
+
+            var radioInput = document.createElement('input');
+
+            radioInput.setAttribute('type', 'radio');
+            radioInput.setAttribute('name', 'cats');
+            radioInput.setAttribute('value', catData.cats[i].name);
+
+            catForm.appendChild(radioInput);
+
+            radioInput.insertAdjacentHTML('afterend', '<label>' + catData.cats[i].name + '</label><br />');
+            
+            
+        }
+
+        
     }
-    
 }
 
-
-
+view.loadCats();
 
 
 /*
@@ -55,11 +75,43 @@ var view = {
 
 var controller = {
 
-    catSwitcher: function(cat) {
 
+    // Switch Cats
+    switchCats: function () {
+
+        var catImage = document.getElementById('cat-pic');
+
+        var inputFields = document.querySelectorAll('#cats-form input[type=radio]');
+
+        // Loop through all input fields
+        for (i = 0; i < inputFields.length; i++) {
+
+            // Set first radio input to checked
+            inputFields[0].setAttribute('checked', '');
+
+            // Update cat name
+            inputFields[i].addEventListener('click', function() {
+                document.getElementById('cat-name').innerHTML = this.value;
+                
+                
+
+                // Check for the FORM RADIO BTN index and match it with the ARRAY index
+                for (k = 0; k < catData.cats.length; k++) {
+                   console.log('i is ' + i + ' and k is ' + k);
+
+                   catImage.setAttribute('src', catData.cats[k].imgURL);
+                }
+                
+            });
+            
+
+        }
         
-    }
+    },
+
 }
 
+
+controller.switchCats();
 
 
--- cats-app/main.js
+++ cats-app/main.js
@@ -4,48 +4,34 @@ $(function () {
     var catImg = $('.cat-switcher');
     var catClicks = $('.updateCounter');
 
-    var clickCounter1 = 0;
-    var clickCounter2 = 0;
-    var clickCounter3 = 0;
-    var clickCounter4 = 0;
-    var clickCounter5 = 0;
-
     $('#cats-form input').change(function () {
 
-        // Get current cat
         var currentCat = $('input[name=cats]:checked').val();
 
-        // Model - Update image and title based on current cat
         switch (currentCat) {
             case 'Cat 1':
                 imgURL = 'imgs/cats-1.jpeg';
                 catName = 'Cat 1';
-                clicks = clickCounter1++;
                 break;
             case 'Cat 2':
                 imgURL = 'imgs/cats-2.jpeg';
                 catName = 'Cat 2';
-                clicks = clickCounter2++;
                 break;
             case 'Cat 3':
                 imgURL = 'imgs/cats-3.jpeg';
                 catName = 'Cat 3';
-                clicks = clickCounter3++;
                 break;
             case 'Cat 4':
                 imgURL = 'imgs/cats-4.jpeg';
                 catName = 'Cat 4';
-                clicks = clickCounter4++;
                 break;
             case 'Cat 5':
                 imgURL = 'imgs/cats-5.jpeg';
                 catName = 'Cat 5';
-                clicks = clickCounter5++;
                 break;
             default: 
                 imgURL = 'imgs/cats-1.jpeg';
                 catName = 'Cat 1';
-                clicks = clickCounter1++;
 
                 console.log(currentCat);
                 
